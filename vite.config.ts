ed1f9ee6387708ec86f351d7bcc2a40c730769b6
rahuldadhich1517-sys <rahuldadhich1517@gmail.com>
2026-08-31 17:16:35 +0530

    Deployment setup

--- vite.config.ts
+++ vite.config.ts
@@ -1,21 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import * as path from 'path';
 
 export default defineConfig({
+  base: '/Rahul-Dadhich-Portfolio/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve('./src'),
-    },
-  },
-  server: {
-    port: 5173,
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-      },
-    },
-  },
 })
